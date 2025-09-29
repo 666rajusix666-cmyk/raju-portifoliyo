f8aeacf43431e67e1454ff860028bc096f803a58
666rajusix666-cmyk <666rajusix666@gmail.com>
2025-09-29 14:44:13 +0600

    a

--- client/src/components/examples/SkillBar.tsx
+++
@@ -1,9 +0,0 @@
-import SkillBar from '../SkillBar';
-
-export default function SkillBarExample() {
-  return (
-    <div className="p-8 bg-background max-w-md">
-      <SkillBar skill="React" percentage={85} />
-    </div>
-  );
-}
--- client/src/components/examples/SocialLinks.tsx
+++
@@ -1,9 +0,0 @@
-import SocialLinks from '../SocialLinks';
-
-export default function SocialLinksExample() {
-  return (
-    <div className="p-8 bg-background">
-      <SocialLinks />
-    </div>
-  );
-}
